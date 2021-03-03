3d04a6e958b3e2e87f38773665b9a8141c40bcfb
Fedorov Konstantin <konstantindigit@gmail.com>
2021-03-03 22:39:12 +0300

    finished 9 video

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 interface TodoListProps {
-    items:Array<{id:string,text: string}>
+    items: Array<{ id: string, text: string }>
+    onDeleteTodo: (id: string) => void
 }
 
 function TodoList(props: TodoListProps) {
     const items = props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null,todo.id)}>DELETE</button>
+        </li>
     ));
     return (
         <ul>
